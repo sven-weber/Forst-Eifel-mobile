8a825e9f35d5825279435709f00bb7c59343ad06
sven-weber <mail@sven-weber.net>
2020-10-13 18:54:07 +0200

    Implementing error check in request

---
+++ lib/wordpress/DTO/wordPressError.dart
@@ -0,0 +1,19 @@
+class WordPressError {
+  ///Error code
+  String code;
+
+  /// Error message
+  String message;
+
+  WordPressError({this.code, this.message});
+
+  WordPressError.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    message = json['message'];
+  }
+
+  @override
+  String toString() {
+    return 'WordPress API error. Code: $code, message. $message';
+  }
+}
--- lib/wordpress/wordpressImpl.dart
+++ lib/wordpress/wordpressImpl.dart
@@ -11,32 +11,92 @@ import 'dart:async';
 import 'constants.dart';
 import 'wordpress.dart';
 import 'DTO/post.dart';
+import 'DTO/wordPressError.dart';
 
 export 'DTO/post.dart';
 export 'DTO/rederObject.dart';
 
-// *****************************************
-// ************ Main Class ***************
-// *****************************************
 class WordPressImpl implements WordPress {
-  String _basePath;
+  // ******** Private Properties **********
+  String _host;
+  String _scheme;
   HttpClient _client;
+  //TODO: Add other method to provide properties
+  List<String> test = [
+    'id',
+    'date',
+    'link',
+    'title',
+    'content',
+    'author',
+    'featured_media'
+  ];
 
+  // ******** Constructor **********
   WordPressImpl({String basePath, HttpClient httpClient}) {
-    this._basePath = basePath;
+    Uri tmp = Uri.parse(basePath);
+    _host = tmp.host;
+    _scheme = tmp.scheme;
     _client = httpClient ?? di.get<HttpClient>();
   }
 
+  // ******** private Methods  **********
+
+  Uri _getUri(String path, Map<String, dynamic> queryParameters) {
+    return Uri(
+        host: _host,
+        scheme: _scheme,
+        path: path,
+        queryParameters: queryParameters);
+  }
+
+  /// Trys to decode the response body using json
+  /// Returns list [jsonObject, errorMsg]
+  /// If decoding failed jsonObject will be null and errorMsg will contain a
+  /// corresponding string
+  Future<List> tryDecodeJsonFromResponse(HttpClientResponse response) async {
+    try {
+      return [json.decode(await response.readAsString()), ''];
+    } catch (e) {
+      return [null, e.toString()];
+    }
+  }
+
+  /// Trys to perform a HTTP request to the provided target using the provided client
+  /// Returns List [HttpClientResponse, errorMsg]
+  /// On Failure response will be null and errorMsg will contain a
+  /// corresponding string
+  Future<List> tryGetUrlWithoutHeader(HttpClient client, Uri target) async {
+    try {
+      return [await client.getUrlWithoutHeader(target), ''];
+    } catch (e) {
+      return [null, e.toString()];
+    }
+  }
+
+  // ******** public Methods  **********
   Future<List<Post>> getPosts() async {
-    final response = await _client.getUrlWithoutHeader(Uri.parse('$_basePath/$URL_PAGES'));
+    Uri target = _getUri('/$URL_PAGES', {'_fields': test.join(',')});
+
+    // Perform request and check for failure
+    var request = await tryGetUrlWithoutHeader(_client, target);
+    if (request[0] == null) throw WordPressError(message: request[1]);
+    HttpClientResponse response = request[0];
+
+    // Try to decode json, check for failure
+    var decode = await tryDecodeJsonFromResponse(response);
+    if (decode[0] == null) throw WordPressError(message: decode[1]);
+    var json = decode[0];
+
     if (response.isSuccessful()) {
+      //Handle successfull request
       List<Post> posts = List();
-      final list = json.decode(await (response.readAsString()));
-      for (final post in list) {
+      for (final post in json) {
         posts.add(Post.fromJson(post));
       }
       return posts;
+    } else {
+      throw WordPressError.fromJson(json);
     }
-    return null;
   }
 }
